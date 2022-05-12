22e805422f2464657861192f25477d5b1518675a
Brandon <btoms.20@gmail.com>
2022-05-12 16:40:38 -0700

    Added a position argument to our SecurityUpgraders upgradeConnection method.

--- Sources/LibP2PNoise/LibP2PNoise.swift
+++ Sources/LibP2PNoise/LibP2PNoise.swift
@@ -12,8 +12,7 @@ public struct NoiseUpgrader: SecurityUpgrader {
     public static let key:String = "/noise"
     let application:Application
     
-    public func upgradeConnection(_ conn: Connection, securedPromise: EventLoopPromise<Connection.SecuredResult>) -> EventLoopFuture<Void> {
-        //print("NOISE::TODO:: Secure this connection!")
+    public func upgradeConnection(_ conn: Connection, position:ChannelPipeline.Position, securedPromise: EventLoopPromise<Connection.SecuredResult>) -> EventLoopFuture<Void> {
         // Given a ChannelHandlerContext Configure and Install our HandshakeHandler onto the pipeline
         let handshake = InboundNoiseHandshakeHandler(
             peerID: conn.localPeer,
@@ -23,11 +22,7 @@ public struct NoiseUpgrader: SecurityUpgrader {
             expectedRemotePeerID: conn.expectedRemotePeer?.b58String
         )
         
-        conn.logger.trace("Searching for BackPressureHandler")
-        return conn.channel.pipeline.handler(type: BackPressureHandler.self ).flatMap { bph in
-            conn.logger.trace("Installing Noise Handshake handler after BackPressureHandler")
-            return conn.channel.pipeline.addHandler( handshake, position: .after(bph) )
-        }
+        return conn.channel.pipeline.addHandler( handshake, position: position )
     }
     
     public func printSelf() {
