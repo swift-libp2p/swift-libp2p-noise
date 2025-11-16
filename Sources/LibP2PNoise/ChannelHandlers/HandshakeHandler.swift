42510c78218ec306ee6da57f140c76a3f67394cf
Brandon <32753167+btoms20@users.noreply.github.com>
2025-11-16 10:11:51 -0800

    fix: added preconcurrency tag to Crypto import to silence swift 6.0 errors

--- Sources/LibP2PNoise/ChannelHandlers/HandshakeHandler.swift
+++ Sources/LibP2PNoise/ChannelHandlers/HandshakeHandler.swift
@@ -12,7 +12,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-import Crypto
+// TODO: Remove preconcurrency tag once we drop support for swift 6.0
+@preconcurrency import Crypto
 import Foundation
 import LibP2PCore
 import Logging
